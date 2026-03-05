3358248ea7367720b2bca485b154e909b4a1241b
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-03-05 19:57:11 +0530

    Continue

--- array/medium/pos_neg.cpp
+++ array/medium/pos_neg.cpp
@@ -6,17 +6,17 @@ int main() {
   int n = sizeof(nums) / sizeof(nums[0]);
 
   int pos = 0;
-  int neg = 1;
+  // int neg = 1;
 
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i = i + 2) {
     if (nums[i] > 0) {
       swap(nums[i], nums[pos]);
       pos = pos + 2;
     }
-    if (nums[i] < 0) {
-      swap(nums[i], nums[neg]);
-      neg = neg + 2;
-    }
+    /* if (nums[i] < 0) {
+       swap(nums[i], nums[neg]);
+       neg = neg + 2;
+     }*/
   }
   for (int i = 0; i < n; i++) {
     cout << nums[i] << " ";
