c97e3a8c39896560a43e52f0a2caa060a1fd4b14
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-03-05 20:28:10 +0530

    Rearrange the postive and negetive elements

--- array/medium/pos_neg_optimal.cpp
+++ array/medium/pos_neg_optimal.cpp
@@ -1,3 +1,4 @@
+// SPACE COMPLEXITY = n
 #include <iostream>
 using namespace std;
 
@@ -5,21 +6,23 @@ int main() {
   int nums[] = {1, 2, -4, -5};
   int n = sizeof(nums) / sizeof(nums[0]);
 
+  int ans[n];
+
   int pos = 0;
   int neg = 1;
 
-  for (int i = 0; i < n; i = i + 2) {
+  for (int i = 0; i < n; i++) {
     if (nums[i] > 0) {
-      swap(nums[i], nums[pos]);
+      ans[pos] = nums[i];
       pos = pos + 2;
     }
     if (nums[i] < 0) {
-      swap(nums[i], nums[neg]);
+      ans[neg] = nums[i];
       neg = neg + 2;
     }
   }
   for (int i = 0; i < n; i++) {
-    cout << nums[i] << " ";
+    cout << ans[i] << " ";
   }
   cout << endl;
 }
