64ff2ce80969c9e536113501c7f84ad52786502c
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-03-25 23:50:06 +0530

    cp

---
+++ array/medium/longest_sumsequnce.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int arr[] = {100, 4, 200, 1, 2, 3};
+  int temp = 0;
+  int ans = 0;
+  int point = 0;
+  for (int i = 0; i < 6; i++) {
+    point = arr[i];
+    for (int j = 0; j < 6; j++) {
+      if (arr[j] == point + 1)
+    }
+  }
+
+  return 0;
+}
