f9ea62f16688e807f4041d635ffd5597738152af
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-03-26 23:37:04 +0530

    Merge Sort

--- sort/merge.cpp
+++ sort/merge.cpp
@@ -1,63 +1,55 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-// 1-2-26
-// Merge Sort Algo Implementation
-// Author : Aman
 
-void merge(vector<int> &arr, int st, int mid, int end){
-		vector<int> temp;
-		int i = st;
-		int j = mid+1;
-		while(i <= mid && j <= end){
-			if(arr[i] <= arr[j]){   // if > then sorting in decending order.
-				temp.push_back(arr[i]);
-				i++;
-			}
-			else{
-				temp.push_back(arr[j]);
-				j++;
-			}
-		}
-		//remaining ones
-		while(i <= mid){
-			temp.push_back(arr[i]);
-			i++;
-		}
-		while(j <= end){
-			temp.push_back(arr[j]);
-			j++;
-		}
-		// from temp to og array
-		for(int idx = 0; idx < temp.size(); idx++){
-			arr[idx + st] = temp[idx];
-		}
+// merge the sorted
+void merge(vector<int> &arr, int start, int mid, int end) {
+  vector<int> temp;
+  int i = start;
+  int j = mid + 1;
+  while (i <= mid && j <= end) {
+    if (arr[i] < arr[j]) {
+      temp.push_back(arr[i]);
+      i++;
+    } else {
+      temp.push_back(arr[j]);
+      j++;
+    }
+  }
 
-}
-// passing array by reference 
-void mergeSort(vector<int> &arr, int st, int end){
-		if(st < end){
-			int mid = st + (end - st)/2; // subtle way of calculating mid
+  // remaining of left
+  while (i <= mid) {
+    temp.push_back(arr[i]);
+    i++;
+  }
+  // remaining of right
+  while (j <= end) {
+    temp.push_back(arr[j]);
+    j++;
+  }
 
-			//left half
-			mergeSort(arr, st, mid);
-			
-			// right half
-			mergeSort(arr,mid+1,end);
-			
-			// calling merge
-			merge(arr,st,mid,end);
-		}
+  // copying to main
+  for (int i = 0; i < temp.size(); i++) {
+    arr[start + i] = temp[i];
+  }
 }
 
-int main(){
+// DIVIDE THE ARRAY TILL ELEMENTRY
+void merge_sort(vector<int> &arr, int start, int end) {
+  if (start < end) {
+    int mid = start + (end - start) / 2;
+    merge_sort(arr, start, mid);
+    merge_sort(arr, mid + 1, end);
+    merge(arr, start, mid, end);
+  }
+}
 
-	vector<int> prr = {30,20,10,25,32,11}; // array initilization
-	mergeSort(prr, 0, prr.size() -1);
-	for(int val : prr){
-		cout << val << " ";
-	}
-	cout <<endl;
-
-	return 0;
+int main() {
+  vector<int> arr = {12, 3, 2, 4, 22, 65, 78, 53, 2, 5};
+  merge_sort(arr, 0, arr.size() - 1);
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+  return 0;
 }
