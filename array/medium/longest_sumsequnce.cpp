c80832e9801396f48c3c1187b10d01605aa46fdc
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-04-05 23:47:46 +0530

    Longest subsequence

--- array/medium/longest_sumsequnce.cpp
+++ array/medium/longest_sumsequnce.cpp
@@ -1,18 +1,57 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+void merge(int arr[], int s, int m, int e) {
+  int i = s;
+  int j = m + 1;
+  vector<int> temp;
 
-  int arr[] = {100, 4, 200, 1, 2, 3};
-  int temp = 0;
-  int ans = 0;
-  int point = 0;
-  for (int i = 0; i < 6; i++) {
-    point = arr[i];
-    for (int j = 0; j < 6; j++) {
-      if (arr[j] == point + 1)
+  while (i <= m && j <= e) {
+    if (arr[i] <= arr[j]) {
+      temp.push_back(arr[i]);
+      i++;
+    } else {
+      temp.push_back(arr[j]);
+      j++;
     }
   }
 
+  while (i <= m) {
+    temp.push_back(arr[i]);
+    i++;
+  }
+
+  while (j <= e) {
+    temp.push_back(arr[j]);
+    j++;
+  }
+
+  for (int i = 0; i < temp.size(); i++) {
+    arr[s + i] = temp[i];
+  }
+}
+
+void divide(int arr[], int s, int e) {
+  if (s < e) {
+    int m = (s + e) / 2;
+    divide(arr, s, m);
+    divide(arr, m + 1, e);
+    merge(arr, s, m, e);
+  }
+}
+
+int main() {
+
+  int arr[] = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+  divide(arr, 0, 9);
+  int ans = 1;
+  int n = sizeof(arr) / sizeof(int);
+  for (int i = 0; i < n - 1; i++) {
+    if (arr[i] + 1 == arr[i + 1]) {
+      ans++;
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
