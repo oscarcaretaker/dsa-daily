8a070d925dcdf6be9f7e8014cd07fa7ed35783b9
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-04-07 21:30:05 +0530

    substring

--- array/medium/longest_sumsequnce.cpp
+++ array/medium/longest_sumsequnce.cpp
@@ -43,15 +43,24 @@ void divide(int arr[], int s, int e) {
 
 int main() {
 
-  int arr[] = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
-  divide(arr, 0, 9);
-  int ans = 1;
+  int arr[] = {1, 2, 3, 5, 6, 7, 8, 9, 10};
   int n = sizeof(arr) / sizeof(int);
-  for (int i = 0; i < n - 1; i++) {
-    if (arr[i] + 1 == arr[i + 1]) {
-      ans++;
+  divide(arr, 0, n - 1);
+
+  int count = 1;
+  int a = arr[0];
+  int ans = 0;
+  for (int i = 1; i <= n - 1; i++) {
+    if (a + 1 == arr[i]) {
+      a = arr[i];
+      count++;
+    } else {
+      a = arr[i];
+      ans = max(count, ans);
+      count = 1;
     }
   }
+
   cout << ans << endl;
   return 0;
 }
