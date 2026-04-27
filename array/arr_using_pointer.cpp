3351e8ef5b450d225eb744417def81026361ef05
oscarcaretaker <connect.with.aman.tiwari@gmail.com>
2026-04-27 23:27:42 +0530

    l

--- array/arr_using_pointer.cpp
+++ array/arr_using_pointer.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	
-	int num[] = {10,20,30,40,50};
-	int *ptr;
-	ptr = num;
-	
-	for(int i = 0; i<5; i++){
-		cout << num[i] <<" -> "<<ptr << endl;
-		ptr++;
-	}
+int main() {
 
-	return 0;
+  int num[] = {10, 20, 30, 40, 50};
+  int *ptr;
+  ptr = num;
+
+  for (int i = 0; i < 5; i++) {
+    cout << num[i] << " -> " << ptr << endl;
+    ptr++;
+  }
+
+  return 0;
 }
